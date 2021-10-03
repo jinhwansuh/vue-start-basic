0bd3cf10c2c539e629883773512ab3996bb58b98
jinhwansuh <nick02190219@gmail.com>
2021-10-04 00:03:32 +0900

    Add router

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,3 @@
 <template>
-  <h1>
-    {{ msg }}
-  </h1>
-  <Hello />
+  <RouterView />
 </template>
-
-<script>
-import Hello from '~/components/Hello'
-
-export default {
-  components: {
-    Hello
-  },
-  data() {
-    return {
-      msg: 'Hello Webpack?'
-    }
-  }
-}
-</script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import App from './App'
 import store from '~/store/index'
+import router from '~/routes/index'
 
 const app = createApp(App)
 app.use(store)
+app.use(router)
 app.mount('#app')
